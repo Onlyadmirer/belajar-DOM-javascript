a136727438f9585fbab3e8836e4d8d2c884d41c9
AkmalHstl <akmalrbc6@gmail.com>
2025-07-09 12:43:26 +0800

    menambahkan image gallery

--- imageGallery/script.js
+++ imageGallery/script.js
@@ -9,12 +9,14 @@ thum.addEventListener('click', function(e) {
   const pickCaption = e.target.dataset.caption;
 
   mainImg.classList.add('opacity-10');
+  caption.classList.add('opacity-10');
 
   setTimeout(() => {
     mainImg.src = url;
     caption.innerHTML = pickCaption
     
     mainImg.classList.remove('opacity-10');
+    caption.classList.remove('opacity-10');
   }, 300);
   };
 
